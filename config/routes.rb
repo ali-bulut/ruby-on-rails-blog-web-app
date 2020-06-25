3217f75be33001db6ecba6a12a976745d539d9a3
Ali <alibulut@yahoo.com>
2020-06-25 18:21:58 +0300

    customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # when we use specific route for all of the operations such as delete, patch, post, get...,  we should use
   # resources. But if we have simple pages for only showing (like about or contact pages) we can simply use get.
-  resources :portfolios
+
+  # that means use default routes for portfolios except show route.
+  resources :portfolios, except: [:show]
+  # so we can create a custom route for show. as => we can set the prefix(route method) of show route.
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   # if we want to reach about page by writing directly (localhost:3000/about instead of localhost:3000/pages/about)
   #get 'pages/about'
