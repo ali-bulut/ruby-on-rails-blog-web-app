b31fbc90d7c0dfc5c931c8df6c8535107e63feaa
Ali <alibulut@yahoo.com>
2020-06-25 18:10:32 +0300

    configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
+  # when we use specific route for all of the operations such as delete, patch, post, get...,  we should use
+  # resources. But if we have simple pages for only showing (like about or contact pages) we can simply use get.
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+
+  # if we want to reach about page by writing directly (localhost:3000/about instead of localhost:3000/pages/about)
+  #get 'pages/about'
+  # we can use this. We use # instead of /
+  get 'about', to: 'pages#about'
+
+  # if we want to react contact page by writing localhost:3000/contact-me
+  #get 'pages/contact'
+  # we can use this. So basically we can write anything we want for routing.
+  get 'contact-me', to: 'pages#contact'
+
+
   resources :blogs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # we can define root path of app like this. We use # instead of /
+  root to: 'pages#home'
 end
