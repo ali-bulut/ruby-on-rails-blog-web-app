7a42d254b7ab6b99660bfd404d500ea5a8eb57b7
Ali <alibulut@yahoo.com>
2020-06-26 18:19:03 +0300

    added new parameters for user registration and editing account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  # it's doing same thing with before_action only difference is that we can choose which controller can reach that.
+  # so here we say if the controller is devise controller lets it reach.
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name])
+  end
 end
