abaed8ed164cf395bc04d98a8970143d58f0aab3
Ali <alibulut@yahoo.com>
2020-06-26 18:56:47 +0300

    completed authentication with virtual attrs for user model

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -1,6 +1,7 @@
 module DeviseWhitelist
   extend ActiveSupport::Concern
 
+  # in order to write before_action method we should use included. That is given us by ActiveSupport.
   included do
     # it's doing same thing with before_action only difference is that we can choose which controller can reach that.
     # so here we say if the controller is devise controller lets it reach.
@@ -8,7 +9,8 @@ module DeviseWhitelist
   end
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
-      devise_parameter_sanitizer.permit(:account_update, keys:[:name])
+    # that is special for devise.
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
