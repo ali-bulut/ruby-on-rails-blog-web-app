88366fbd8a92fed5499e4133aebe7d57e1de46c3
Ali <alibulut@yahoo.com>
2020-06-26 18:39:17 +0300

    added controller concerns for devise params

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,14 @@
+module DeviseWhitelist
+  extend ActiveSupport::Concern
+
+  included do
+    # it's doing same thing with before_action only difference is that we can choose which controller can reach that.
+    # so here we say if the controller is devise controller lets it reach.
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  end
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+      devise_parameter_sanitizer.permit(:account_update, keys:[:name])
+  end
+end
