5ea4424d3bad4ecd1aad12d81e5df2f3c142faaa
Ali <alibulut@yahoo.com>
2020-06-26 11:05:42 +0300

    implemented validations for blogs, skills and portfolio items

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,7 @@ class Blog < ApplicationRecord
   enum status: {draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  # by writing this we are banning that adding a new blog or editing an existing blog without title and body text.
+  validates_presence_of :title, :body
 end
