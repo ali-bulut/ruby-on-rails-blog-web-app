0f8e5ad2fd09f067e5e60699b456bc1dd8c42642
Ali <alibulut@yahoo.com>
2020-06-30 19:49:09 +0300

    updated file upload forms and refactored placeholder functionality into view

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,8 +6,8 @@ class Portfolio < ApplicationRecord
   # we can create new techologies inside of the Portfolio query.
   # that means => do not accept technologies if name attr of technology is empty (this is just a validation.)
   accepts_nested_attributes_for :techologies, reject_if: lambda { |attrs| attrs["name"].blank? }
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  #include Placeholder
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -25,14 +25,14 @@ class Portfolio < ApplicationRecord
 
   # that means => after 'new'(not create!) method in controller works, the portfolio form will be created.
   # So after form created, this method will run.
-  after_initialize :set_defaults
+  #after_initialize :set_defaults
 
-  def set_defaults
+  ### def set_defaults
     # ||= => self.main_image = "https://via.placeholder.com/600x400" if main_image == nil
     # ||= => that means if you have already a main_image or thumb_image I dont override them.
     # but if you use = instead, that means when we have already an main_image or thumb_image,
     # ruby will not care about that and it will override that.
-    self.main_image ||= Placeholder.image_generator(600,400)
-    self.thumb_image ||= Placeholder.image_generator(350,200)
-  end
+  ###  self.main_image ||= Placeholder.image_generator(600,400)
+  ###  self.thumb_image ||= Placeholder.image_generator(350,200)
+  ### end
 end
